d13ca9f7cb4dea5f7603bf1155da7eda66338ba9
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-06-06 17:18:23 -0300

    Inicio de tarea -Interfaz

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,15 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+interface ITarea {
+    fun horasNecesariasParaFinalizarTarea(): Int
+    fun costoDeUnaTarea(): Double
+    fun calcularSalarioTotalDeEmpleados(): Double
+    fun darNominaDeEmpleados() : MutableList<Empleado>
+}
+
+class Empleado(var cobraPorHora : Double){
+    fun saberCuantoCobraPorHora(): Double = cobraPorHora
+    fun calcularCuantoCobraPorCantidadDeHorasTrabajadas( cantidadDeHoras : Int): Double {
+        return cantidadDeHoras * saberCuantoCobraPorHora()
+    }
+}

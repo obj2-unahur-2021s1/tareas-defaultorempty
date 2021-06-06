79971b5e239d23019e4e6382169ce80ce137e2df
sdelafuente <coalsan@gmail.com>
2021-06-06 18:29:16 -0300

    Test Tarea Integracion con tarea simple

--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -4,30 +4,57 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
-  describe("Una tarea simple") {
+    describe("Una tarea simple") {
 
-    var responsable = Empleado(500.0)
-    var empleadoUno = Empleado(300.0)
-    var empleadoDos = Empleado(300.0)
+        var responsable = Empleado(500.0)
+        var empleadoUno = Empleado(300.0)
+        var empleadoDos = Empleado(300.0)
 
-    var listaDeEmpleadosTareaSimple = mutableListOf<Empleado>()
-    listaDeEmpleadosTareaSimple.add(empleadoUno)
-    listaDeEmpleadosTareaSimple.add(empleadoDos)
+        var listaDeEmpleadosTareaSimple = mutableListOf<Empleado>()
+        listaDeEmpleadosTareaSimple.add(empleadoUno)
+        listaDeEmpleadosTareaSimple.add(empleadoDos)
 
-    var tareaSimple = TareaSimple(100,responsable, listaDeEmpleadosTareaSimple, 50000)
+        var tareaSimple = TareaSimple(100, responsable, listaDeEmpleadosTareaSimple, 50000)
 
-    it("dar nomina de empleados"){
-      tareaSimple.darNominaDeEmpleados().count().shouldBe(3)
+        it("dar nomina de empleados") {
+            tareaSimple.darNominaDeEmpleados().count().shouldBe(3)
+        }
+
+        it("calcular cantidad de horas necesarias para finalizar la tarea") {
+            tareaSimple.horasNecesariasParaFinalizarTarea().shouldBe(50)
+        }
+
+        it("obtener el costo de una tarea") {
+            tareaSimple.costoDeUnaTarea().shouldBe(130000)
+        }
     }
 
-    it("calcular cantidad de horas necesarias para finalizar la tarea"){
-      tareaSimple.horasNecesariasParaFinalizarTarea().shouldBe(50)
+    describe("Una tarea Integracion con una tarea simple") {
+
+        var responsable = Empleado(600.0)
+        var responsableIntegracion = Empleado(800.0)
+        var empleadoUno = Empleado(250.0)
+        var empleadoDos = Empleado(400.0)
+
+        var listaDeEmpleadosTareaSimple = mutableListOf<Empleado>()
+        listaDeEmpleadosTareaSimple.add(empleadoUno)
+        listaDeEmpleadosTareaSimple.add(empleadoDos)
+
+        var tareaSimple = TareaSimple(32, responsable, listaDeEmpleadosTareaSimple, 70000)
+        var tareaIntegracion = TareaIntegracion(responsableIntegracion)
+        tareaIntegracion.listaDeTareas.add(tareaSimple)
+
+        it("dar nomina de empleados") {
+            tareaIntegracion.darNominaDeEmpleados().count().shouldBe(4)
+        }
+
+        it("calcular cantidad de horas necesarias para finalizar la tarea") {
+            tareaIntegracion.horasNecesariasParaFinalizarTarea().shouldBe(18)
+        }
+
+        it("obtener el costo de una tarea") {
+            tareaIntegracion.costoDeUnaTarea().shouldBe(102588)
+        }
     }
 
-    it("obtener el costo de una tarea"){
-
-      //50000+15000+15000+50000
-      tareaSimple.costoDeUnaTarea().shouldBe(130000)
-    }
-  }
 })

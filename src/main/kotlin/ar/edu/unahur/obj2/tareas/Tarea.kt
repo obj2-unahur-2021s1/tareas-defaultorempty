e523446d9193f93e189b410303e645ecd42f5f64
sdelafuente <coalsan@gmail.com>
2021-06-06 17:21:24 -0300

    Se agrega class tarea simple.

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -4,12 +4,41 @@ interface ITarea {
     fun horasNecesariasParaFinalizarTarea(): Int
     fun costoDeUnaTarea(): Double
     fun calcularSalarioTotalDeEmpleados(): Double
-    fun darNominaDeEmpleados() : MutableList<Empleado>
+    fun darNominaDeEmpleados(): MutableList<Empleado>
 }
 
-class Empleado(var cobraPorHora : Double){
+class TareaSimple(
+    var cantidadHorasEstimadas: Int,
+    var responsable: Empleado,
+    var empleados: MutableList<Empleado>,
+    var costoInfraestructura: Int
+) : ITarea {
+    override fun horasNecesariasParaFinalizarTarea(): Int {
+        return cantidadHorasEstimadas / empleados.size
+    }
+
+    override fun costoDeUnaTarea(): Double {
+        return costoInfraestructura + this.calcularSalarioTotalDeEmpleados() + responsable.calcularCuantoCobraPorCantidadDeHorasTrabajadas(
+            cantidadHorasEstimadas
+        )
+    }
+
+    override fun calcularSalarioTotalDeEmpleados(): Double {
+        return empleados.sumByDouble { it.calcularCuantoCobraPorCantidadDeHorasTrabajadas(this.horasNecesariasParaFinalizarTarea()) }
+    }
+
+    override fun darNominaDeEmpleados(): MutableList<Empleado> {
+        var nominaEmpleados = empleados
+        nominaEmpleados.add(responsable)
+        return nominaEmpleados
+    }
+
+}
+
+class Empleado(var cobraPorHora: Double) {
     fun saberCuantoCobraPorHora(): Double = cobraPorHora
-    fun calcularCuantoCobraPorCantidadDeHorasTrabajadas( cantidadDeHoras : Int): Double {
+    fun calcularCuantoCobraPorCantidadDeHorasTrabajadas(cantidadDeHoras: Int): Double {
         return cantidadDeHoras * saberCuantoCobraPorHora()
     }
 }
+
